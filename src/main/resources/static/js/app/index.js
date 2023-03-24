14e4f8bb4b5d5c0eda436b48e80f7c62fa853406
PowerOleg <geraskin.oleg@mail.ru>
2023-03-24 15:28:34 +0500

    Thymeleaf lessons in members.html

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,4 +1,35 @@
-function substituteOnClick(foo) {
-//    $('#c').replaceWith('<h5 id="c">Tag was substituted</h5>')
-console.log('aaa')
+function MyFunction() {
+    alert('The example has worked out'); return false
+}
+
+function ChangeColor(Element) {
+if (Element.style.color == 'green') Element.style.color = 'red';
+	else Element.style.color = 'green';
+	return false;
+}
+
+
+function Substitute() {
+//var name = document.getElementById("onclick").value;          !!!
+
+
+    // create the new element (input)
+    var textBox = document.createElement("input");
+    textBox.type = "text";
+    // get the button
+    var button = document.getElementById("onclick");
+    // replace it
+    button.replaceWith(textBox);
+}
+
+function replaceWithText(text) {
+    // get the button
+    var button = document.getElementById("4");
+    // replace it
+    button.replaceWith(text);
+}
+
+function add(a, b) {
+      var sum = parseInt(a, 10) + parseInt(b, 10);
+      alert(sum);
 }
